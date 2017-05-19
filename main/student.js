244b7d1757d43eaffa443835be370626e118b0c6
leloucj <gothicj@163.com>
2017-05-19 21:27:21 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -2,11 +2,11 @@ let Person = require('./person')
 // Write your code here
 module.exports = Student
 function Student(name,age,class){
-Person.call(this,name)
-  Person.call(this,age)
+Person.apply(this,arguments)
   this.class=class
     this.introduce=function(){
     var intro = "My name is "+this.name+". I am "+this.age+" years old. I am a Student. I am at Class "+this.class+"."
     return intro
     }
 }
+//Student.prototype = new Person()
