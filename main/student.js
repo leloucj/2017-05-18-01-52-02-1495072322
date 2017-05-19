af84d26136245cc96c77fb1e9f07183f332b3442
leloucj <gothicj@163.com>
2017-05-19 21:35:01 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -5,7 +5,7 @@ function Student(name,age,kclass){
 Person.apply(this,arguments)
   this.kclass=kclass
     this.introduce=function(){
-    var intro = "My name is "+this.name+". I am "+this.age+" years old. I am a Student. I am at Class "+this.class+"."
+    var intro = "My name is "+this.name+". I am "+this.age+" years old. I am a Student. I am at Class "+this.kclass+"."
     return intro
     }
 }
