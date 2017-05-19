75f245f615502700c42b5ef07898f5aab4bf27f7
leloucj <gothicj@163.com>
2017-05-19 20:49:21 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,2 +1,11 @@
 // Write your code here
 module.exports = Person
+function person(name,age){
+this.mane = name
+  this.age = age
+  introduce:function(){
+    var intro ="My name is "+this.name+". I am "+this.age+" years old."
+    return intro
+  }
+}
+
