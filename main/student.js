e2bb47e469ed999a027bc223c71a88ac56e855c7
leloucj <gothicj@163.com>
2017-05-19 21:04:48 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -4,7 +4,7 @@ module.exports = Student
 function Student(name,age,class){
 Person.call(this,name,age)
   this.class=class
-    introduce:function(){
+    introduce=function(){
     var intro = "My name is "+this.name+". I am "+this.age+" years old. I am a Student. I am at Class "+this.class+"."
     return intro
     }
